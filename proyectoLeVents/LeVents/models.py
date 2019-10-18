e45c0a8b71a37f436c2f4d6e30028bb4019b59cd
Emilio <emi.rojasc@alumnos.duoc.cl>
2019-10-18 10:44:51 -0300

    template base

--- proyectoLeVents/LeVents/models.py
+++ proyectoLeVents/LeVents/models.py
@@ -21,10 +21,17 @@ class Post(models.Model):
             return self.title
 
 class Usuario(models.Model):
-        run = models.ForeignKei('auth.User', on_delete=models.CASCADE)
+        run = models.CharField(max_length=200 )
         nombre = models.CharField(max_length=40)
         apellido_paterno = models.CharField(max_length=40)
         apellido_materno = models.CharField(max_length=40)
         user = models.CharField(max_length=40)
         password = models.CharField(max_length=50)
+
+        def publish(self):
+            self.save()
+        
+        def __str__(self):
+            return self.run
+        
         
