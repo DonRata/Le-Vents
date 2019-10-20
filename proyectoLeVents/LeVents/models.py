6f80b274aef7574857ca27dc98d3bccfe017fae9
Emilio <emilio.r.cruz@gmail.com>
2019-10-20 01:45:53 -0300

    Clase post eliminada

--- proyectoLeVents/LeVents/models.py
+++ proyectoLeVents/LeVents/models.py
@@ -2,29 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django import forms
 
-
-
-class Post(models.Model):
-        author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-        title = models.CharField(max_length=200)
-        text = models.TextField()
-        created_date = models.DateTimeField(
-            default=timezone.now)
-        published_date = models.DateTimeField(
-            blank=True, null=True)
-
-        def publish(self):
-            self.published_date = timezone.now()
-            self.save()
-
-        def __str__(self):
-            return self.title
-
 class Usuario(models.Model):
         run = models.CharField(max_length=200 )
-        nombre = models.CharField(max_length=40)
-        apellido_paterno = models.CharField(max_length=40)
-        apellido_materno = models.CharField(max_length=40)
+        nombre_completo = models.CharField(max_length=200)
+        email = models.CharField(max_length=100)
+        fecha_nacimiento = models.DateField()
+        telefono_contacto = models.CharField(max_length=15)
         user = models.CharField(max_length=40)
         password = models.CharField(max_length=50)
 
