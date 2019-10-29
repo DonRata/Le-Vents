c4f182aaf77ff4e4ff4de4c96ce36cc48769e72c
Emilio <emilio.r.cruz@gmail.com>
2019-10-29 19:49:24 -0300

    Cambios del alvaro fixeados

--- proyectoLeVents/LeVents/views.py
+++ proyectoLeVents/LeVents/views.py
@@ -1,14 +1,20 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from django.shortcuts import render
+# -----------  para nuestos modelos --------------------
+from .models import Usuario, Marca, Genero, Tipo, Producto, Boleta, Venta
 
-# Create your views here.
+
 def page_list(request):
     return render(request, 'LeVents/pagina_principal.html', {})
 
-def productos_hombre(request):
-	productos = Productos.objects.get(id_genero=1)
 
-    return render(request, 'LeVents/productos_hombre.html', {'productos': productos})
+def productos_hombre(request):
+    productos = Producto.objects.all()
+    return render(request, "LeVents/productos_hombre.html", {'productos': productos})
+
 
 def productos_mujer(request):
     return render(request, 'LeVents/productos_mujer.html', {})
--- proyectoLeVents/mysite/urls.py
+++ proyectoLeVents/mysite/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('LeVents.urls')),
+    path('productos_hombre', include('LeVents.urls')),
+    path('productos_mujer', include('LeVents.urls')),
+    path('quienes_somos', include('LeVents.urls')),
 ]
