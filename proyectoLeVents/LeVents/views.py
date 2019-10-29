03ad9e57a9556fe61a8022b865ae839fa134dddc
alvaro <alv.riquelmec@alumnos.duoc.cl>
2019-10-29 18:51:41 -0300

    view prod_h y template prod_h

--- proyectoLeVents/LeVents/views.py
+++ proyectoLeVents/LeVents/views.py
@@ -6,7 +6,9 @@ def page_list(request):
     return render(request, 'LeVents/pagina_principal.html', {})
 
 def productos_hombre(request):
-    return render(request, 'LeVents/productos_hombre.html', {})
+	productos = Productos.objects.get(id_genero=1)
+
+    return render(request, 'LeVents/productos_hombre.html', {'productos': productos})
 
 def productos_mujer(request):
     return render(request, 'LeVents/productos_mujer.html', {})
