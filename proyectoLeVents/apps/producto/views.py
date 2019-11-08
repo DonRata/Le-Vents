f0b7467a9a3d69557ed86811fd3c9a1af66b0a7f
Emilio <emilio.r.cruz@gmail.com>
2019-11-08 01:41:02 -0300

    Implementacion permisos por perfiles

--- proyectoLeVents/apps/producto/views.py
+++ proyectoLeVents/apps/producto/views.py
@@ -8,25 +8,63 @@ def pagina_principal(request):
     categorias = Categoria.objects.all()
     return render(request, 'pagina_principal.html', {'categorias': categorias})
 
+
 def producto_agregar(request):
-    if request.method == 'POST':
-        form = ProductoForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/productos_listado/')
+    user = request.user
+    if user.has_perm('apps.moderador'):
+        if request.method == 'POST':
+            form = ProductoForm(request.POST)
+            if form.is_valid():
+                form.save()
+            return redirect('/productos_listado/')
+        else:
+            form = ProductoForm()
+        return render(request, 'producto/producto_agregar.html', {'form': form})
     else:
-        form = ProductoForm()
-    return render(request, 'producto/producto_agregar.html', {'form': form})
+        return redirect('/')
 
 def producto_listado(request):
-	producto = Producto.objects.all().order_by('id')
-	return render(request, 'producto/productos_listado.html', {'producto':producto})
+    producto = Producto.objects.all().order_by('id')
+    user = request.user
+    if user.has_perm('apps.moderador'):
+        return render(request, 'producto/productos_listado.html', {'producto':producto})
+    else:
+        return redirect('/') 
 
 def listar_productos(request, id_url):
-    #categorias = Categoria.objects.all().filter(id_categoria=id_categoria)
+    # categorias = Categoria.objects.all().filter(id_categoria=id_categoria)
     if id_url:
         productos = Producto.objects.all().filter(categoria_id=id_url+1)
     return render(request, 'producto/productos_listar.html', {'productos': productos})
+
+def editar_producto(request, id_prod):
+    producto = Producto.objects.get(id_producto=id_prod)
+    user = request.user
+    if user.has_perm('apps.moderador'):
+        if request.method == "GET":
+            form = ProductoForm(instance=producto)
+        else:
+            form = ProductoForm(request.POST, instance=producto)
+            if form.is_valid():
+                producto = form.save(commit=False)
+                producto.save()
+            return redirect('/productos_listado/')
+        return render(request, "producto/productos_editar.html", {'form': form})
+    else:
+        return redirect('/') 
+
+
+def borrar_producto(request, id_prod):
+    producto = Producto.objects.get(id_producto=id_prod)
+    user = request.user
+    if user.has_perm('apps.moderador'):
+        if request.method == 'POST':
+            producto.delete()
+            return redirect('/productos_listado/')
+        return render(request, 'producto/producto_eliminar.html', {'producto': producto})
+    else:
+        return redirect('/') 
+
 
 class ProductoListado(ListView):
     model = Producto
