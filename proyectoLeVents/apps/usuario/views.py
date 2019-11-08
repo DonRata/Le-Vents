73ecaa5dc6a1d9032df5aae6ebfd7e5294c946cd
Emilio <emilio.r.cruz@gmail.com>
2019-11-08 01:59:00 -0300

    Implementacion registro exitoso

--- proyectoLeVents/apps/usuario/views.py
+++ proyectoLeVents/apps/usuario/views.py
@@ -15,13 +15,16 @@ from .serializers import UserSerializer
 def quienes_somos(request):
 	qsomos = Imagenes_Pagina.objects.all()
 	return render(request, 'quienes_somos.html', {'qsomos': qsomos} )
+
+def registro_exitoso(request):
+	return render(request, 'usuario/registro_exitoso.html')
 	
 
 class RegistroUsuario(CreateView):
     model = User
     template_name = "usuario/registro.html"
     form_class = RegistroForm
-    success_url = reverse_lazy('producto:pagina_principal')
+    success_url = '/accounts/registro_exitoso'
 
 
 class UserAPI(APIView):
